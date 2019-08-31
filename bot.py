6ce9b98be40d988054d9f676d529e8451c732089
KelvinMwinuka <KelvinMwinuka@hotmail.co.uk>
2019-08-31 03:45:49 +0200

    Added database functionality (Saving new records and updating status of existing records)

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import time
 import json
 from tinydb import TinyDB, Query
 
+
 def main():
 
     db = TinyDB('job_applications.json')
@@ -57,7 +58,26 @@ def main():
     except NoSuchElementException:
         print("End of the jobs list")
 
+    updated_jobs = []
 
+    # Traverse each job card and extract job information
+    for card in job_cards:
+        title = card.find_element_by_class_name("mdc-typography--subtitle1").text
+        company = card.find_element_by_class_name("mdc-typography--subtitle2").text
+        status = card.find_element_by_class_name("mdc-chip__text").text
+        date = card.find_element_by_class_name("ja-created-at").text
+
+        # Check if the job exists in the database, if not, insert it.
+        jobs = db.search((Job.title == title) and (Job.company == company) and (Job.date == date))
+        if len(jobs) < 1:
+            db.insert({'title': title, 'company': company, 'status': status, 'date': date})
+            # Send sms/email notification of the new job insertion
+        else:
+            # If the job exists, check if the status has been updated and add to the 'updated' list accordingly
+            if jobs[0]['status'] != status:
+                db.update({'status': status}, (Job.title == title) and (Job.company == company) and (Job.date == date))
+                # Send sms/email notification of update
+                pass
 
     time.sleep(10)
     driver.quit()
